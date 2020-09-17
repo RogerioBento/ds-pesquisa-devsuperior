ecd12064fceccfc24e3be16a6e4c7ee245b64888
Rogerio Bento <rogeriokjrjr@hotmail.com>
2020-09-17 20:26:37 -0300

    Adding charts page

--- front-web/src/routes.tsx
+++ front-web/src/routes.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Header from './components/header';
+import Charts from './pages/charts';
 import Home from './pages/home';
-import Records from './pages/home/records';
+import Records from './pages/records';
 
 const Routes = () => (
  <BrowserRouter>
@@ -14,6 +15,10 @@ const Routes = () => (
 
         <Route path="/records">
             <Records />
+        </Route>
+
+        <Route path="/charts">
+            <Charts />
         </Route>
 
     </Switch>
