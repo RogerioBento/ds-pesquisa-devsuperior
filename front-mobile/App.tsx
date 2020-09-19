177a8c397c2b495790cc0e6575f34188742e83f1
Rogerio Bento <rogeriokjrjr@hotmail.com>
2020-09-19 10:09:33 -0300

    routes on react native

--- front-mobile/App.tsx
+++ front-mobile/App.tsx
@@ -8,7 +8,7 @@ import {
   Play_700Bold
 } from '@expo-google-fonts/play';
 import Header from './src/components/header'
-import Home from './src/pages/Home'
+import Routes from './src/routes'
 
 export default function App() {
   let [fontsLoaded] = useFonts({
@@ -21,8 +21,7 @@ export default function App() {
   } else {
     return (
       <View style={styles.container}>
-        <Header />
-        <Home />
+        <Routes />
          <StatusBar style="auto" />
       </View>
     );
@@ -31,8 +30,7 @@ export default function App() {
   
   const styles = StyleSheet.create({
     container: {
-      flex: 1,
-      backgroundColor: "#0B1F34"
+      flex: 1
     }
   });
 
